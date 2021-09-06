16bc271923ac7204883a190697d180ea0f50de5f
jisneoit <jinseoit@gmail.com>
2021-09-06 22:31:26 +0900

    server initialize

--- TAW_app/src/components/admin/uploadPage.js
+++ TAW_app/src/components/admin/uploadPage.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const uploadPage = () => {
   return (
-    <div>
-        <h2>업로드 페이지</h2>
-    </div>
+    <>
+      <h2>업로드 페이지</h2>
+      <input type="text"/>
+    </>
   )
 }
 
